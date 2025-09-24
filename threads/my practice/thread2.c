6c3ef2ff8a785fb0a5ea4375ac9853c3d5a6f8a1
adityapaul26 <adityapaul2603@gmail.com>
2025-09-24 11:14:38 +0530

    Auto commit 2025-09-24 11:14:38

--- threads/my practice/thread2.c
+++ threads/my practice/thread2.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<sys/types.h>
 #include<pthread.h>
 #include<unistd.h>
--- threads/my practice/thread3.c
+++ threads/my practice/thread3.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<pthread.h>
-#include<stdlib.h>
 #include<unistd.h>
 
 typedef struct{
